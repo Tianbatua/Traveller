46920f1c3585fdec6d13e79ddc5721f7a9d405af
Brian Cai <mingcancai@gmail.com>
2018-04-05 13:41:57 -0400

    server side Registration MongoDB Model

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
-const config = require('./config/database');
-const path = require('path');
+const config = require('./config/database'); //monggose config
+const path = require('path'); //package for file paths
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
 
+
+// Database Connection
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
 	if(err){
@@ -13,12 +18,20 @@ mongoose.connect(config.uri, (err) => {
 	}
 });
 
+// Provide static directory for frontend
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) 
+// parse application/json
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/client/dist/'));
+app.use('/authentication', authentication);
 
+// Connect server to Angular Index.html
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
+// Start Server: Listen on port 8080
 app.listen(8080, () => {
 	console.log('listening on port 8080');
 });
